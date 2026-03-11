f04932776ea9764a64755ed9c8a45c4de1d2edaa
Gina Rubik <ginarubik@gmail.com>
2026-03-11 23:06:51 +0100

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package hu.progmasters.rex;
-
-public class Main {
-    public static void main(String[] args) {
-        Engine engine = new Engine();
-        engine.run();
-    }
-}
-                    
