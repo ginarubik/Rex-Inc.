32ec96ed9113d120eb771f7407b16aab24eedd2f
Gina Rubik <ginarubik@gmail.com>
2026-03-11 23:15:40 +0100

    Add abstract Pet class with properties and methods

--- pets/Pet.java
+++ pets/Pet.java
@@ -1,1 +1,22 @@
+package hu.progmasters.rex.pets;
 
+public abstract class Pet {
+
+    private String name;
+    private int age;
+    private boolean cleansed;
+
+    public Pet(String name, int age){
+        this.name = name;
+        this.age = age;
+        this.cleansed = false;
+    }
+
+    public boolean isCleansed() {
+        return cleansed;
+    }
+
+    public void setCleansed(boolean cleansed) {
+        this.cleansed = cleansed;
+    }
+}
