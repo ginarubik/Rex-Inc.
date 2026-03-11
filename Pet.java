41e67972a53355d11c8deb34c0230410330284d5
Gina Rubik <ginarubik@gmail.com>
2026-03-11 16:33:57 +0100

    Refactor Pet class constructor and methods

--- Pet.java
+++ Pet.java
@@ -1,28 +1,22 @@
+package hu.progmasters.rex.pets;
+
 public abstract class Pet {
 
     private String name;
     private int age;
     private boolean cleansed;
 
-    protected Pet(String name, int age) {
+    public Pet(String name, int age){
         this.name = name;
         this.age = age;
         this.cleansed = false;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
     }
 
     public boolean isCleansed() {
         return cleansed;
     }
 
-    public void cleanse() {
-        this.cleansed = true;
+    public void setCleansed(boolean cleansed) {
+        this.cleansed = cleansed;
     }
 }
